4d2032f14507b997211f3ddb400c11ae3786d44a
AdnanTurjoy <aturjoy4@gmail.com>
2022-10-28 01:05:18 +0600

    notes complete

--- App.js
+++ App.js
@@ -1,11 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  View,
+  ScrollView,
+  Platform,
+  Keyboard,
+} from "react-native";
+import Note from "./components/Note";
 
 export default function App() {
+  const [note, setNote] = useState("");
+  const [noteItems, setNoteItems] = useState([]);
+  const [modal,setModal] = useState(false);
+  // Add Note
+  const handleAddnote = () => {
+    Keyboard.dismiss();
+    setNoteItems([...noteItems, note]);
+    setNote("");
+  };
+// delete note
+  const deleteNote = (index) => {
+    setModal(true);
+    setNoteItems(noteItems.filter((data, id) => id !== index));
+    setTimeout(()=>{
+      setModal(false);
+    },2000)
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ScrollView
+        contentContainerStyle={{
+          flexGrow: 1,
+        }}
+        keyboardShouldPersistTaps="handled"
+      >
+        {/* notes */}
+        <View style={styles.notesWrapper}>
+          <Text style={styles.sectionTitle}>Notes</Text>
+          {modal && <Text style={styles.removeText}>Successfully Deleted</Text> }
+          <View style={styles.items}>
+            {noteItems.map((item, index) => {
+              return <Note text={item} index={index} deleteNote={deleteNote} />;
+            })}
+          </View>
+        </View>
+      </ScrollView>
+
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.writenoteWrapper}
+      >
+        <TextInput
+          style={styles.input}
+          placeholder={"Write a note"}
+          value={note}
+          onChangeText={(text) => setNote(text)}
+        />
+        <TouchableOpacity onPress={() => handleAddnote()}>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
 }
@@ -13,8 +73,53 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#E8EAED",
   },
+  notesWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  removeText:{
+      fontSize:15,
+      color:'red',
+      backgroundColor:"#f9cdc4",
+      padding: 10,
+      borderRadius: 15
+      
+  },
+  items: {
+    marginTop: 30,
+  },
+  writenoteWrapper: {
+    position: "absolute",
+    bottom: 60,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: "#FFF",
+    borderRadius: 60,
+    borderColor: "#C0C0C0",
+    borderWidth: 1,
+    width: 250,
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: "#FFF",
+    borderRadius: 60,
+    justifyContent: "center",
+    alignItems: "center",
+    borderColor: "#C0C0C0",
+    borderWidth: 1,
+  },
+  addText: {},
 });
